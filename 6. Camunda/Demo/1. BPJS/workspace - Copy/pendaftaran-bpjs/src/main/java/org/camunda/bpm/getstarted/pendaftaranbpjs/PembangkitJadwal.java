95171b3a3288a5b1b96bb3c734b3d788fec7087f
luckysenjaya <luczsenjaya@yahoo.co.id>
2017-04-27 06:28:09 +0700

    Demo Stable

---
+++ 6. Camunda/Demo/1. BPJS/workspace - Copy/pendaftaran-bpjs/src/main/java/org/camunda/bpm/getstarted/pendaftaranbpjs/PembangkitJadwal.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.pendaftaranbpjs;
+
+public class PembangkitJadwal {
+
+	public String jadwalKedatangan(){
+		
+		return "Senin, 77 Juli 7777, Kliwon";
+	}
+	
+	public int nomorAntrian(){
+		return 1;
+	}
+}

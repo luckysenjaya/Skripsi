0f60fda8b44498cb517fbd954e54eec7dcc05852
luckysenjaya <luczsenjaya@yahoo.co.id>
2017-05-04 01:29:27 +0700

    Buku Skripsi

--- 6. Camunda/Demo/1. BPJS/workspace - Copy/pendaftaran-bpjs/src/main/java/org/camunda/bpm/getstarted/pendaftaranbpjs/PembangkitBarcode.java
+++
@@ -1,8 +0,0 @@
-package org.camunda.bpm.getstarted.pendaftaranbpjs;
-
-public class PembangkitBarcode {
-
-	public int getBarcode(){
-		return 12345;
-	}
-}
--- 6. Camunda/Demo/1. BPJS/workspace - Copy/pendaftaran-bpjs/src/main/java/org/camunda/bpm/getstarted/pendaftaranbpjs/PendaftaranBPJS.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.getstarted.pendaftaranbpjs;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("PendaftaranBPJS App")
-public class PendaftaranBPJS extends ServletProcessApplication {
-	
-}
